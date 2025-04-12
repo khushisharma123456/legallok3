9d345ddb13485c0769dee4466796e72b5ea6eafc
khushisharma123456 <144713310+khushisharma123456@users.noreply.github.com>
2025-04-12 18:38:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 import os
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
 
 
 app = Flask(__name__)
